9549604f875af07314a708bc8ae8c06c9d148c7e
bendo <andrasgit@gmail.com>
2025-02-27 05:18:00 +0000

    version+

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 pushd Bendo.SimpleDataFrameBuilder
+rm bin/Release/Bendo.SimpleDataFrameBuilder.*.nupkg
 dotnet pack --configuration Release
 dotnet nuget push bin/Release/Bendo.SimpleDataFrameBuilder.*.nupkg --source https://api.nuget.org/v3/index.json --api-key $NUGET_API_KEY
 popd
